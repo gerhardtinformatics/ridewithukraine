faa2764b68a9872a632b23471f9e3567bb068abb
Kiss Daniel <dki@gerhardt.io>
2022-03-24 14:43:18 +0100

    META-677 made changes in css and php file

--- trips.php
+++ trips.php
@@ -49,7 +49,7 @@
         }
 
           echo '<td id="link">';
-          echo "<a href='$links'>Show</a>";
+          echo "<a id='button1' href='$links' >Show</a>";
           echo '</td>';
           }
 
